01a7dd4027c3a91a599a77cdeb2a24c3d26d528a
Seongguk <live8681@naver.com>
2020-09-08 17:21:18 +0900

    project copy

---
+++ TestBlazorCoreMvc.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestBlazorCoreMvc.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
